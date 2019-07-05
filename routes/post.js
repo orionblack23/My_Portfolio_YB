4763c3019cee3037a930c60463b472c14a07bc74
ORION BLACK <orionb.y23@gmail.com>
2019-07-05 13:00:15 -0700

    fixing display bugs

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/post', (req, res, next) => { 
-    res.render('post');
+    res.render('post',{
+        navbar: {brandName: 'YASSER'}, 
+        section7: {
+            headingTitle: 'Contact',
+            email: 'orionb.y23@gmail.com',
+            phone: '+90 533 836 13 68',
+            social1: 'https://github.com/orionblack23',
+            social1Font: 'fa-github',
+            social2: 'https://www.linkedin.com/in/yasser-belemlougri-a3672610b/',
+            social2Font: 'fa-linkedin'
+          }
+    });
 });
 
 module.exports = router;
