8369d7a2bbca72866042970db5156329dc7805ab
ORION BLACK <orionb.y23@gmail.com>
2019-07-05 04:50:52 -0700

    modifying education and skills section

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
+    pageTitle: 'Yasser BELEMLOUGRI',
     navbar: {brandName: 'YASSER'}, 
     section1: {
       title: 'Yasser BELEMLOUGRI',
@@ -24,10 +25,35 @@ router.get('/', function(req, res, next) {
       dateOfBirth: '23 September 1996',
       email: 'orionb.y23@gmail.com',
       phone: '+90 533 836 13 68',
-      city: 'Lefke, Northen Cyprus',
-      interest1: 'Data mining',
-      interest1Icon: 'n',
+      city: 'Lefke, Northen Cyprus'
+    },
+    section3: {
+      headingTitle: 'Education',
+      header: 'Computer Engineering',
+      subHeader: 'European University of Lefke / 2016 - 2019',
+      content: 'Receved one High Honor and 5 Honor award/Certificate. Finish with a CGPA of 3.35/4.00',
+      header1: 'Computer Engineering',
+      subHeader1: 'Meliksha University / 2015 - 2016',
+      content1: '',
+      header2: 'High School',
+      subHeader2: 'College Horizon International / 2007 - 2014',
+      content2: '',
 
+    },
+    section4: {
+      headingTitle: 'Skills',
+      skill: 'C/C++',
+      level: '75',
+      skill1: 'HTML 5',
+      level1: '80',
+      skill2: 'CSS 3',
+      level2: '70',
+      skill3: 'JavaScript',
+      level3: '85',
+      skill4: 'WordPress',
+      level4: '70',
+      skill5: 'SQL',
+      level5: '80',
     }
    });
 });
